4d8132048775ee38fe7efb7d7c263bc38cba4319
SayliMN <saylinarkhede@yahoo.com>
2023-05-30 22:58:24 -0700

    complete sign in functionality

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,10 @@
 import { useState } from "react"
 import {AiFillEyeInvisible, AiFillEye} from "react-icons/ai"
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import OAuth from "../components/OAuth";
+import { signInWithEmailAndPassword, getAuth} from "firebase/auth";
+import { toast } from "react-toastify";
+
 export default function SignIn() {
     const [showPassword, setShowPassword] = useState(false);
     const [formData, setFormData] = useState({
@@ -9,11 +12,26 @@ export default function SignIn() {
         password: "",
     });
     const {email, password} = formData;
+    const navigate = useNavigate();
+
     function onChange(e){
         setFormData((prevState) => ({
             ...prevState,
             [e.target.id]: e.target.value,
         }))
+    }
+
+    async function onSubmit(e) {
+        e.preventDefault()
+        try {
+            const auth = getAuth()
+            const userCredential = await signInWithEmailAndPassword(auth, email, password)
+            if(userCredential.user) {
+                navigate("/")
+            }
+        } catch (error) {
+            toast.error("Bad user credentials!")
+        }
     }
     return (
         <section> 
@@ -25,7 +43,7 @@ export default function SignIn() {
                     className="w-full rounded-2xl"/>
                 </div>
                 <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
-                    <form>
+                    <form onSubmit={ onSubmit }>
                         <input type="email" id="email" value={email} onChange={onChange} placeholder="Email address" className="mb-6 w-full px-4 py-2 text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"/>
                         <div className="relative mb-6">
                             <input type={showPassword ? "text" : "password"} 
